556bae39b3821bee7788089f0da4249d5f96084f
DwiTeguhImanto <70831556+DwiTeguhImanto@users.noreply.github.com>
2020-10-11 23:14:11 +0700

    ci4 minggu ke 6
    video 42

--- ci4/app/Controllers/Admin/User.php
+++ ci4/app/Controllers/Admin/User.php
@@ -1,52 +1,30 @@
-<?php namespace App\Controllers\Admin;
-use App\Controllers\BaseController;
+<?php 
 
+namespace App\Controllers\Admin;
+use App\Controllers\BaseController;
+use App\Models\User_M;
 
 class User extends BaseController
 {
 
-    protected $session=null;
-
-    public function __construct()
-    {
-        $this->session = \config\Services::session();
-    }
+   
 
 	public function index()
 	{
-		echo "user";
+		$pager = \Config\Services::pager();
+		$model = new User_M();
+
+
+		$data = [
+			'judul' => 'DATA USER',
+			'user' => $model->paginate(3, 'page'),
+            'pager' => $model->pager
+
+		];
+		return view("user/select",$data);
+	
+
 	}
 	
-	public function create()
-    {   
-
-        $tbluser = [
-            'user' => 'chefku',
-            'email' => 'chefku@gmail.com',
-            'level' => 'chef'
-        ];
-
-        $this->session->set($tbluser);
-    }
-
-    public function read()
-    {
-        echo $this->session->get('email');
-        echo "<br>";
-        echo $this->session->get('user');
-        echo "<br>";
-        echo $this->session->get('level');
-    }
-
-    public function delete()
-    {
-        $this->session->remove('email');
-    }
-
-    public function destroy()
-    {
-    
-        $this->session->destroy();
-    }
 
 }
---
+++ ci4/app/Models/User_M.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+use CodeIgniter\Model;
+
+class User_M extends  Model
+{
+    // read db
+    protected $table = 'tbluser';
+
+}
+
+
+?>
