58e25af56af321d2f1d5d6ef5a8cb979ad61f3de
DwiTeguhImanto <70831556+DwiTeguhImanto@users.noreply.github.com>
2020-10-21 21:35:05 +0700

    belajar ci4 minggu ke 7
    video 50

--- ci4/app/Controllers/Admin/login.php
+++ ci4/app/Controllers/Admin/login.php
@@ -49,7 +49,7 @@ class Login extends BaseController
     public function logout()
     {
         session()->destroy();
-        return redirect()->to(base_url('/login'));
+        return redirect()->to(base_url('/dashboard'));
     }
 
 	//--------------------------------------------------------------------
